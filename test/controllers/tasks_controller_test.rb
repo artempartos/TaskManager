6380ebbe4d95e17f3f2dcddf7acdc27aa790d730
Morozov Nikita <nikmorze@gmail.com>
2016-06-05 19:45:28 +0400

    Old hash format was removed. Configuration for test datebase was added. Factories were added. Integration and model tests were removed. Simple form gem was added.

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
   setup do
-    @task = generate :task
-    @inserted_task = Task.new(generate :task).save
+    @task = create :task
+    @attrs = attributes_for :task
   end
 
   test "should get index" do
@@ -19,30 +19,31 @@ class TasksControllerTest < ActionController::TestCase
 
   test "should create task" do
     assert_difference('Task.count') do
-      post :create, task: @task
+      post :create, task: @attrs
     end
 
     assert_redirected_to task_path(assigns(:task))
   end
 
   test "should show task" do
-    get :show, id: @inserted_task
+
+    get :show, id: @task.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @inserted_task
+    get :edit, id: @task.id
     assert_response :success
   end
 
   test "should update task" do
-    patch :update, id: @inserted_task, task: @task
+    patch :update, id: @task.id, task: @attrs
     assert_redirected_to task_path(assigns(:task))
   end
 
   test "should destroy task" do
     assert_difference('Task.count', -1) do
-      delete :destroy, id: @inserted_task
+      delete :destroy, id: @task.id
     end
 
     assert_redirected_to tasks_path
--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,15 +1,6 @@
-# encoding: utf-8
 FactoryGirl.define do
-  sequence :task do |n|
-    {
-      "title" => "Title very long title #{n}",
-      "description" => "description #{n}"
-    }
-  end
-  sequence :task_for_update do |n|
-    {
-        "title" => "Title very long title updated#{n}",
-        "description" => "description updated#{n}"
-    }
+  factory :task do
+    title
+    description
   end
 end
