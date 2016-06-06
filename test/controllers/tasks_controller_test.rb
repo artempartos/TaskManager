049b3063f5cffd52fa577bb5836e7a85bac4dfeb
Morozov Nikita <nikmorze@gmail.com>
2016-06-06 13:11:33 +0400

    form attributes were removed. assert_not was removed, form validation errors block was removed

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -9,7 +9,6 @@ class TasksControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:tasks)
   end
 
   test "should get new" do
